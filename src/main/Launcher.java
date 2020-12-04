e5001117af51a904621a1c76d24b0a2bbfce79b4
Michael <michael.li.web@gmail.com>
2020-12-03 21:19:52 -0500

    Video testing purposes

--- src/main/Launcher.java
+++ src/main/Launcher.java
@@ -3,6 +3,6 @@ package main;
 public class Launcher {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Hello, World! is overrated");
+//        System.out.println("Hello, World! is overrated"); there shall only be one hello world here :/
     }
 }
