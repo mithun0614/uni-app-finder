902b7cf9c50e7c116da92a1338cd64090e9728f6
Soup321 <75334988+Soup321@users.noreply.github.com>
2020-12-03 21:16:15 -0500

    Testing for Video purposes (#1)

--- src/main/Launcher.java
+++ src/main/Launcher.java
@@ -3,5 +3,6 @@ package main;
 public class Launcher {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Hello, World! is overrated");
     }
 }
