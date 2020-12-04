50c8742ba7b94c9459f2689012efe3d725fc930a
jeffreyjxang <ouraize2003@gmail.com>
2020-12-04 13:22:02 -0500

    Added csv files, and test file to print

---
+++ src/main/Test.java
@@ -0,0 +1,50 @@
+package main;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        String[] names = new String[14];
+        String[] col1 = new String[14];
+        Scanner input;
+
+
+
+        try {
+            input = new Scanner(new File("dataTables/(CSV) Secondary School Averages 2016.csv")); //Input from Treasures file
+            input.useDelimiter(","); //Knows when next item starts
+            int index = 0;
+            int arrayIndex = 0;
+            int col1ArrayIndex = 0;
+            while (input.hasNext()) { //Gets every input from text file
+                String value = input.next();
+                if (index%9 == 0) {
+                    names[arrayIndex] = value;
+                    arrayIndex++;
+                }
+                if (index%9 == 1) {
+                    col1[col1ArrayIndex] = value;
+                    col1ArrayIndex++;
+                }
+
+
+                index++;
+            }
+            input.close();
+            for (String name : names) {
+                System.out.print(name + " ");
+            }
+
+            System.out.println("95+");
+            for (String value : col1) {
+                System.out.print(value + ", ");
+            }
+        } catch (FileNotFoundException e) { //Safety Check
+            System.out.println("FILE ERROR");
+        }
+
+
+    }
+}
