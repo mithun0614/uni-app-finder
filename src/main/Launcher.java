acc20d449b227c383c2aa1400ae58f2343c42a98
mithun0614 <mithunsivapathasundram@outlook.com>
2020-12-07 21:12:58 -0500

    Added Shrek

--- src/main/Launcher.java
+++ src/main/Launcher.java
@@ -4,6 +4,7 @@ public class Launcher {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello, World! is overrated");
+        System.out.println("shrek");
         // >:))
     }
 }
